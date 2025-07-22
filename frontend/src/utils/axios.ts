803ac8548f981de792dcdf51cffcea0665a2a529
JASIR BASHEER <jasirbinbasheerpp@gmail.com>
2025-07-22 17:57:47 +0530

    fix: env import issue in axios instance

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customAxios = axios.create({
-    baseURL: process.env.VITE_BACKEND || 'http://localhost:5050',
+    baseURL: import.meta.env.VITE_BACKEND || 'http://localhost:5050',
     withCredentials:true
   });
 
